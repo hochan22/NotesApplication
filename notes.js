1f88fd35196feb0f2227b796a3072bdb4e055ce5
HoChan <haochenx22@gmail.com>
2018-10-23 10:17:47 -0700

    Notes Application First Commit

---
+++ notes.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }
+    catch(e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if(duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+};
+
+var getAll = () => {
+    return fetchNotes();
+};
+
+var removeNote = (title) => {
+    // fetch notes
+    var oldNotes = fetchNotes();
+    // filter notes, removing the one with title of argument
+    var filteredNotes = oldNotes.filter((note) => note.title !== title);
+    // save new notes array
+    saveNotes(filteredNotes);
+    return filteredNotes.length !== oldNotes.length;
+};
+
+var getNote = (title) => {
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
+};
+
+var logNote = (note) => {
+    debugger;
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    removeNote,
+    getNote,
+    logNote
+}
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'Hao'
+}
+
+person.age = 25;
+
+debugger;
+
+person.name = 'Mike';
+
+console.log(person);
